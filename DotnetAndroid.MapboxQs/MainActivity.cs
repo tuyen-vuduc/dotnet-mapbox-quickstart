ef6cf18cd424f2ccf8341673333a8dc72bdb1352
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-07-18 06:11:29 +0700

    feat(android): Add MapView to the activity

--- DotnetAndroid.MapboxQs/MainActivity.cs
+++ DotnetAndroid.MapboxQs/MainActivity.cs
@@ -1,13 +1,44 @@
-﻿namespace DotnetAndroid.MapboxQs;
+﻿using AndroidX.AppCompat.App;
+using Com.Mapbox.Maps;
 
-[Activity(Label = "@string/app_name", MainLauncher = true)]
-public class MainActivity : Activity
+namespace DotnetAndroid.MapboxQs;
+
+[Activity(Label = "@string/app_name", MainLauncher = true, Theme = "@style/Theme.AppCompat.Light.DarkActionBar")]
+public class MainActivity : AppCompatActivity
 {
+    MapView mapView;
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
+
+        mapView = FindViewById<MapView>(Resource.Id.mapView);
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        mapView?.OnStart();
+    }
+
+    protected override void OnStop()
+    {
+        base.OnStop();
+        mapView?.OnStop();
+    }
+
+    public override void OnLowMemory()
+    {
+        base.OnLowMemory();
+        mapView?.OnLowMemory();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        mapView?.OnDestroy();
     }
 }
