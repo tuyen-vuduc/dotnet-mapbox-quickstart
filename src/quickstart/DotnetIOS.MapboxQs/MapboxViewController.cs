7ae0af09bc652db510ff4b915aa4d610ba79b0c6
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-02-21 07:56:10 +0700

    - upgrade nuget version

--- src/quickstart/DotnetIOS.MapboxQs/MapboxViewController.cs
+++ src/quickstart/DotnetIOS.MapboxQs/MapboxViewController.cs
@@ -11,16 +11,19 @@ public partial class MapboxViewController : UIViewController
     {
         base.ViewDidLoad();
 
+        MapboxCommon.MBXMapboxOptions.SetAccessTokenForToken(MAPBOX_ACCESS_TOKEN);
+
         // Use all default options, except access_token
-        var resouceOptions = new MBMResourceOptions(
-            MAPBOX_ACCESS_TOKEN,
-            null, null, null, null);
-        var options = MapInitOptionsFactory.CreateWithResourceOptions(
-            resouceOptions, null, null, null, null
+        var mapboxOptions = new MBMMapOptions(null, null, null, null, null, null, 1, null);
+
+        var options = MapInitOptionsFactory.CreateWithMapOptions(
+            mapboxOptions, null, null, null, 0
             );
 
         var mapView = MapViewFactory.CreateWithFrame(View.Bounds, options);
         mapView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+
+        //mapView.LoadStyleWithStyleUriString(BuiltInStyles.Standard, new MBMStylePackLoadOptions(), null, null);
 
         View.AddSubview(mapView);
     }
