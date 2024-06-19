e86ee1617622c891135d2f0d4a533ed14aa31f29
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-06-20 05:23:13 +0700

    Initial iOS samples project

---
+++ src/ios-samples/Mapbox4DotnetIosSamples/AppDelegate.cs
@@ -0,0 +1,23 @@
+﻿namespace Mapbox4DotnetIosSamples;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		// create a new window instance based on the screen size
+		Window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+		Window.RootViewController = new MapboxViewController(); ;
+
+		// make the window visible
+		Window.MakeKeyAndVisible ();
+
+		return true;
+	}
+}
+
